2988a41ef27ae4a51b4f050e899bc93315e843e5
nsymyt <otuyamayihsin.4@gmail.com>
2020-08-05 14:02:48 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all  # すべてのレコードを@postsに代入
-    
-    def new
-    end
-
-    def create
-      Post.create(content: params[:content])
-    end
-
   end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+
 
 end
